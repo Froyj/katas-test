a97357912f05daaa4e084ff5fd598b054e0e3b5d
Geoffroy <geoffroy.periot.dev@gmail.com>
2020-02-15 20:38:47 +0100

    ...

--- loops-with-js/assets/part1.js
+++ loops-with-js/assets/part1.js
@@ -1,11 +1,7 @@
 // Write a loop that display every numbers between 0 and 10 (included) in ASCENDING order
 
-function countFrom0To10() {
-  const a = []
-  for (let i = 0; i <= 10; i++) {
-    a.push(i)
-  }
-  return a
+function sayHello() {
+
 }
 
 // Write a loop that display every numbers between 0 and 10 (included) in ASCENDING order
