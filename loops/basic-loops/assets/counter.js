999c540fd761bf0856590faf6ddcdca9767e4ded
Geoffroy <geoffroy.periot.dev@gmail.com>
2020-02-17 15:12:02 +0100

    making course

--- loops/basic-loops/assets/counter.js
+++ loops/basic-loops/assets/counter.js
@@ -1,12 +1,9 @@
-// Write a loop that display every numbers between 0 and 10 (included) in ASCENDING order
-// Ecrire une boucle qui affiche les nombres de 0 à 10 dans le terminal dans l'ordre croissant
+// Compter la somme des chiffres compris entre 0 et n
 
-// Same thing in DESCENDING order
-// Faire la même mais dans l'ordre décroissant
-
-// Write a loop that display all even numbers
-// Ecrire une loop qui affiche les nombre pairs entre 0 et 20
-
-function counter() {
-
+function counter(n) {
+  let res = 0;
+  for (let i = 1; i <= 10; i++) {
+    res += i;
+  }
+  return res;
 }
--- loops/basic-loops/assets/counter.test.js
+++ loops/basic-loops/assets/counter.test.js
@@ -1,12 +1,5 @@
-// Write a loop that display every numbers between 0 and 10 (included) in ASCENDING order
-// Ecrire une boucle qui affiche les nombres de 0 à 10 dans le terminal dans l'ordre croissant
+const { counter } = require('./iterate-on-array');
 
-// Same thing in DESCENDING order
-// Faire la même mais dans l'ordre décroissant
-
-// Write a loop that display all even numbers
-// Ecrire une loop qui affiche les nombre pairs entre 0 et 20
-
-function counter() {
-
-}
+test('counter', () => {
+  expect(counter(10)).toBe(55);
+});
