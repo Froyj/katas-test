e6b1e90b8cb41514679e321d198b97252df67e59
Geoffroy <geoffroy.periot.dev@gmail.com>
2020-02-19 12:49:43 +0100

    test exercice suite

--- loops/basic-loops/assets/ascendingCounter.js
+++ loops/basic-loops/assets/ascendingCounter.js
@@ -1,21 +1,11 @@
 // Compter la somme des chiffres compris entre 0 et n
-function counter(n) {
-  let res = 0;
-
+for (let i = 0; i <= 10; i++) {
+  console.log(i);
 }
 
-// Faire des exercices obligeant à parcourir une boucle en décrémentant
-
-
-// Ecris une fonction permettant de savoir si le nombre est "rangé",
-// un nombre rangé est un nombre dont les chiffres sont dans l'ordre croissant de gauche à droite
-function findIfTidy() {
-
-}
 
 // Ecrire une fonction permettant de trouver combien de fois une lettre appait dans la phrase cible
-function getLetterOccurences() {
+// function getLetterOccurences() {
 
-}
+// }
 
-module.exports = { counter, counterDesc, findIfTidy, getLetterOccurences };
