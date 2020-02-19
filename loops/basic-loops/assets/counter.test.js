142a2166fc127ab0381ff706e58ed0e629122a84
Geoffroy <geoffroy.periot.dev@gmail.com>
2020-02-19 09:06:10 +0100

    fisrt test ava

--- loops/basic-loops/assets/counter.test.js
+++ loops/basic-loops/assets/counter.test.js
@@ -1,18 +1,20 @@
 const test = require('ava');
 const { counter, counterDesc, findIfTidy, getLetterOccurences } = require('./counter');
 
-test('counter', () => {
-  expect(counter(10)).toBe(55);
+test('counter', t => {
+  const expected = 55
+  const actual = counter(10)
+  t.is(expected, actual);
 });
 
-test('counterDesc', () => {
-  expect(counter(10)).toBe(55);
-});
+// test('counterDesc', () => {
+//   expect(counter(10)).toBe(55);
+// });
 
-test('findIfTidy', () => {
-  expect(counter(10)).toBe(55);
-});
+// test('findIfTidy', () => {
+//   expect(counter(10)).toBe(55);
+// });
 
-test('getLetterOccurences', () => {
-  expect(counter(10)).toBe(55);
-});
+// test('getLetterOccurences', () => {
+//   expect(counter(10)).toBe(55);
+// });
