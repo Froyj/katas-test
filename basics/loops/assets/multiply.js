75cadc2a31099918567ca51b293b4c6cbbcb67d4
Geoffroy Periot <geoffroy@wildcodeschool.fr>
2020-02-25 18:47:03 +0100

    added more steps

--- basics/loops/assets/multiply.js
+++ basics/loops/assets/multiply.js
@@ -2,10 +2,9 @@ function multiply(a, b) {
   let result = 0;
   // write your code below this line
   for(let i=1; i<=a; i++) {
-    result += b;
+    result = b;
   }
   return result
 }
 
-const assert = require('assert').strict;
-assert.strictEqual(multiply(4, 5), 20, 'bravo');
+module.exports = multiply;
---
+++ basics/loops/assets/multiply.test.js
@@ -0,0 +1,4 @@
+const multiply = require('./multiply');
+
+const assert = require('assert').strict;
+assert.strictEqual(multiply(4, 5), 20);
