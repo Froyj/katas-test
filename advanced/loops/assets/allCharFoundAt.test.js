986d2f40784c7e35efac60b550c237fd0acb93c7
Geoffroy Periot <geoffroy@wildcodeschool.fr>
2020-02-26 12:05:30 +0100

    tada

--- advanced/loops/assets/allCharFoundAt.test.js
+++ advanced/loops/assets/allCharFoundAt.test.js
@@ -9,7 +9,7 @@ assert.deepStrictEqual({
   l: [5],
   a: [6],
   t: [7]
-}, charAllFoundAt('chocolat'));
+}, allCharFoundAt('chocolat'));
 console.log("\x1b[32m%s\x1b[0m", "OK // test passed");
 
 console.log("\x1b[1m\x1b[31m")
@@ -19,5 +19,5 @@ assert.deepStrictEqual({
   r: [2, 9],
   c: [4],
   d: [6]
-}, charAllFoundAt('abracadabra'));
+}, allCharFoundAt('abracadabra'));
 console.log("\x1b[32m%s\x1b[0m", "OK // test passed");
