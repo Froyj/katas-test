6286067dc683691f546f38e55ef450702d7abe70
Geoffroy Periot <geoffroy@wildcodeschool.fr>
2020-02-26 10:27:35 +0100

    added exercises in advanced loops

--- advanced/arrays-loops/assets/checkIfInArray.test.js
+++ advanced/arrays-loops/assets/checkIfInArray.test.js
@@ -1,5 +1,5 @@
 const assert = require('assert');
 const checkIfInArray = require('./checkIfInArray');
-console.log("\x1b[1m\x1b[31m\x1b[0m")
-assert.strictEqual(checkIfInArray(12, [true, 12, "hello"]), true);
+console.log("\x1b[1m\x1b[31m")
+assert.strictEqual(checkIfInArray(12, [true, 12, "hello"]), true, "\x1b[1m\x1b[31mNUL\x1b[0m");
 console.log("\x1b[32m%s\x1b[0m", "OK // test passed");
