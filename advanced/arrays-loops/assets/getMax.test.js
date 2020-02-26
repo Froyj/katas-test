c8d8b9723dd7acf2b3c9755e1784b67a9d028cab
Geoffroy Periot <geoffroy@wildcodeschool.fr>
2020-02-26 10:32:37 +0100

    added exercises in advanced loops

--- advanced/arrays-loops/assets/getMax.test.js
+++ advanced/arrays-loops/assets/getMax.test.js
@@ -1,6 +1,6 @@
 const assert = require('assert');
-const checkIfInArray = require('./checkIfInArray');
+const getMax = require('./getMax');
 
 console.log("\x1b[1m\x1b[31m")
-assert.strictEqual(checkIfInArray(12, [true, 12, "hello"]), true);
+assert.strictEqual(getMax([32, 12, 0, 5, -4]), true, error.message);
 console.log("\x1b[32m%s\x1b[0m", "OK // test passed");
