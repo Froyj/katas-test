42e6d2664c65adf701a696d635320ce31b98941f
Geoffroy Periot <geoffroy@wildcodeschool.fr>
2020-02-26 12:03:40 +0100

    tada

--- advanced/loops/assets/allCharFoundAt.js
+++ advanced/loops/assets/allCharFoundAt.js
@@ -1,6 +1,4 @@
-// à partir d'un mot construire un objet
-
-const charAllFoundAt = str => {
+function allCharFoundAt(str) {
   const result = {};
   for (let i = 0 ; i < str.length ; i++) {
     const char = str[i];
@@ -14,25 +12,4 @@ const charAllFoundAt = str => {
   return result;
 }
 
-const assert = require('assert');
-
-console.log("\x1b[1m\x1b[31m");
-assert.deepStrictEqual({
-  c: [0, 3],
-  h: [1],
-  o: [2, 4],
-  l: [5],
-  a: [6],
-  t: [7]
-}, charAllFoundAt('chocolat'));
-console.log("\x1b[32m%s\x1b[0m", "OK // test passed");
-
-console.log("\x1b[1m\x1b[31m")
-assert.deepStrictEqual({
-  a: [0, 3, 5, 7, 10],
-  b: [1, 8],
-  r: [2, 9],
-  c: [4],
-  d: [6]
-}, charAllFoundAt('abracadabra'));
-console.log("\x1b[32m%s\x1b[0m", "OK // test passed");
+module.exports = allCharFoundAt;
--- advanced/loops/assets/allCharFoundAt.test.js
+++ advanced/loops/assets/allCharFoundAt.test.js
@@ -1,6 +1,7 @@
 const assert = require('assert');
 const allCharFoundAt = require('./allCharFoundAt');
 
+console.log("\x1b[1m\x1b[31m");
 assert.deepStrictEqual({
   c: [0, 3],
   h: [1],
@@ -8,12 +9,15 @@ assert.deepStrictEqual({
   l: [5],
   a: [6],
   t: [7]
-}, allCharFoundAt('chocolat'));
+}, charAllFoundAt('chocolat'));
+console.log("\x1b[32m%s\x1b[0m", "OK // test passed");
 
+console.log("\x1b[1m\x1b[31m")
 assert.deepStrictEqual({
   a: [0, 3, 5, 7, 10],
   b: [1, 8],
   r: [2, 9],
   c: [4],
   d: [6]
-}, allCharFoundAt('abracadabra'));
+}, charAllFoundAt('abracadabra'));
+console.log("\x1b[32m%s\x1b[0m", "OK // test passed");
